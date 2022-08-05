b2834804008ea5d1b744e18d44b02b0a428f51e6
nimonirmal <110668226+nimonirmal@users.noreply.github.com>
2022-08-05 16:45:05 +0530

    Add files via upload

---
+++ githubstart.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import os 
+
+
+# In[4]:
+
+
+os.getcwd()
+
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[9]:
+
+
+pd.read_csv('Student.tsv',sep="\t")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
